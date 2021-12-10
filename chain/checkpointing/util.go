da2250b7c1f90532f8cf1d9e56adcffde7ca1d94
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2021-12-10 08:46:17 -0300

    Fix rpc url attempt

--- chain/checkpointing/util.go
+++ chain/checkpointing/util.go
@@ -252,7 +252,7 @@ func GetTxOut(txid string, index int) (float64, []byte) {
 func jsonRPC(payload string) map[string]interface{} {
 	// ZONDAX TODO
 	// This needs to be in a config file
-	url := "btc-rpc"
+	url := "http://btc-rpc"
 	method := "POST"
 
 	user := "satoshi"
